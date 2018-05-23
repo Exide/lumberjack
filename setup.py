dbb7455ac8b1590f478473f9375f993b8f4fe28d
Ryan Zander <exide@hotmail.com>
2018-05-22 18:04:28 -0700

    Followed the official packaging guide.

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-NAME = 'lumberjack'
-DESCRIPTION = 'A logging package for Python.'
-URL = 'https://github.com/Exide/lumberjack'
-EMAIL = 'exide@hotmail.com'
-AUTHOR = 'Ryan Zander'
-REQUIRES_PYTHON = '>=2.7.0'
-VERSION = '0.0.1'
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='lumberjack',
+    version='0.0.1',
+    author='Ryan Zander',
+    author_email='exide@hotmail.com',
+    description='A logging package for Python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Exide/lumberjack',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
